d64b65796227289d68953a7ecac1d3b3cd93cec1
yugal687 <talk2meyugal1996@gmail.com>
2022-06-09 08:08:50 +0545

    thursday june 9 8:00am

--- backend/cart/serializers.py
+++ backend/cart/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Cart
+from item.serializers import ItemSerializer
 # from rest_framework_jwt.settings import api_settings
 # from django.contrib.auth.models import User
 
 class CartSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(read_only=True, many=True)
     class Meta:
         model = Cart
         fields = ('__all__')
